02d2d2cfcf6393129f8303de7b1044d614b6c4a2
Billy Kwok <xobkwok@gmail.com>
2019-09-28 13:27:42 +0800

    basic support for transaction and add Prettier

--- functions/src/models/Trip.ts
+++ functions/src/models/Trip.ts
@@ -1,102 +1,102 @@
 import * as admin from 'firebase-admin';
 import * as User from './User';
+import { Currency } from './Currency';
 
-export type Currency  = 'HKD' | 'JPY' | 'RMB' | 'USD';
 type TripStatus = 'active' | 'archived';
 
 export interface ITrip {
-    id?: string;
-    name: string;
-    user_id: string;
-    status: TripStatus;
-    currency: Currency | null;
-    created_at?: Date;
-    updated_at?: Date;
+  id?: string;
+  name: string;
+  user_id: string;
+  status: TripStatus;
+  currency: Currency | null;
+  created_at?: Date;
+  updated_at?: Date;
 }
 
 export interface ITripHistory {
-    user_id: string;
-    trip_id: string;
+  user_id: string;
+  trip_id: string;
 }
 
 const db = admin.firestore();
 
 export function getCollection() {
-    return db.collection('trip')
+  return db.collection('trip');
 }
 
 export async function findByName(name: string) {
-    const snapshot = await getCollection().where('name', '==', name).limit(1).get();
-    if (!snapshot.empty) {
-        return snapshot.docs[0].data() as ITrip;
-    }
-    return null;
+  const snapshot = await getCollection()
+    .where('name', '==', name)
+    .limit(1)
+    .get();
+  if (!snapshot.empty) {
+    return snapshot.docs[0].data() as ITrip;
+  }
+  return null;
 }
 
 export async function create(params: {
-    user_id: string;
-    name: string;
-    currency?: Currency;
-}){
-    return {
-        ...params,
-        currency: params.currency || null,
-        status: 'active' as TripStatus,
-        created_at: new Date(),
-        updated_at: new Date(),
-    }
+  user_id: string;
+  name: string;
+  currency?: Currency;
+}) {
+  return {
+    ...params,
+    currency: params.currency || null,
+    status: 'active' as TripStatus,
+    created_at: new Date(),
+    updated_at: new Date()
+  };
 }
 
 export async function save(trip: ITrip) {
-    if (trip.id) {
-        await getCollection().doc(trip.id).update({
-            ...trip, 
-            updated_at: new Date()
-        })
-    } else {
-        await getCollection().add({
-            ...trip,
-            created_at: new Date(),
-            updated_at: new Date(),
-        })
-    }
+  if (trip.id) {
+    await getCollection()
+      .doc(trip.id)
+      .update({
+        ...trip,
+        updated_at: new Date()
+      });
+  } else {
+    await getCollection().add({
+      ...trip,
+      created_at: new Date(),
+      updated_at: new Date()
+    });
+  }
 }
 
 export async function findOrCreateTrip(params: {
-    email: string,
-    name: string,
-    currency?: Currency
+  email: string;
+  name: string;
+  currency?: Currency;
 }) {
-    const {
-        email, 
-        name,
-        currency,
-    } = params;
-    const user = await User.findByEmail(email)
-    if (!user) {
-        throw new Error('ERR_ENTITY_NOT_FOUND');
-    }
-    if (await findByName(name)) {
-        throw new Error('ERR_DUPLICATE_KEY');
-    }
-    const tripToSave: ITrip = await create({
-        user_id: user.id!,
-        name,
-        currency
-    })
+  const { email, name, currency } = params;
+  const user = await User.findByEmail(email);
+  if (!user) {
+    throw new Error('ERR_ENTITY_NOT_FOUND');
+  }
+  if (await findByName(name)) {
+    throw new Error('ERR_DUPLICATE_KEY');
+  }
+  const tripToSave: ITrip = await create({
+    user_id: user.id!,
+    name,
+    currency
+  });
 
-    console.log('user');
-    console.log(tripToSave);
+  console.log('user');
+  console.log(tripToSave);
 
-    await save(tripToSave);
-    return findByName(name);
+  await save(tripToSave);
+  return findByName(name);
 }
 
 export async function joinTrip(params: {
-    userName: string,
-    email: string,
-    tripName: string,
+  userName: string;
+  email: string;
+  tripName: string;
 }) {
-    console.log('hi');
+  console.log('hi');
 }
-
--- functions/src/models/User.ts
+++ functions/src/models/User.ts
@@ -1,75 +1,75 @@
 import * as admin from 'firebase-admin';
 
 export interface IUser {
-    id?: string;
-    email: string;
-    name?: string;
-    created_at?: Date;
-    updated_at?: Date;
+  id?: string;
+  email: string;
+  name?: string;
+  created_at?: Date;
+  updated_at?: Date;
 }
 
 const db = admin.firestore();
 
 export function getCollection() {
-    return db.collection('user')
+  return db.collection('user');
 }
 
-
 export async function findBy(key: string, value: string) {
-    const snapshot = await getCollection().where(key, '==', value).limit(1).get();
-    if (!snapshot.empty) {
-        const doc = snapshot.docs[0]
-        return { ...doc.data(), id: doc.id } as IUser;
-    }
-    return null;
+  const snapshot = await getCollection()
+    .where(key, '==', value)
+    .limit(1)
+    .get();
+  if (!snapshot.empty) {
+    const doc = snapshot.docs[0];
+    return { ...doc.data(), id: doc.id } as IUser;
+  }
+  return null;
 }
 
 export async function findByEmail(email: string) {
-    return findBy('email', email);
+  return findBy('email', email);
 }
 
 export async function findByName(name: string) {
-    return findBy('name', name);
+  return findBy('name', name);
 }
 
-
-export async function create(params: {
-    email: string;
-    name: string;
-}) {
-    return {
-        ...params,
-        created_at: new Date(),
-        updated_at: new Date(),
-    }
+export async function create(params: { email: string; name: string }) {
+  return {
+    ...params,
+    created_at: new Date(),
+    updated_at: new Date()
+  };
 }
 
 export async function save(user: IUser) {
-    if (user.id) {
-        await getCollection().doc(user.id).update({
-            ...user, 
-            updated_at: new Date()
-        })
-    } else {
-        await getCollection().add({
-            ...user,
-            created_at: new Date(),
-            updated_at: new Date(),
-        })
-    }
+  if (user.id) {
+    await getCollection()
+      .doc(user.id)
+      .update({
+        ...user,
+        updated_at: new Date()
+      });
+  } else {
+    await getCollection().add({
+      ...user,
+      created_at: new Date(),
+      updated_at: new Date()
+    });
+  }
 }
 
 export async function findOrCreateUser(params: {
-    name: string,
-    email: string,
+  name: string;
+  email: string;
 }) {
-    let user: IUser | null = await findByEmail(params.email);
-    if (user) {
-        return user;
-    } else {
-        const userToSave = await create(params);
-        await save(userToSave)
-        user = await findByEmail(params.email);
-        return user;
-    }
+  let user: IUser | null = await findByEmail(params.email);
+  if (user) {
+    return user;
+  } else {
+    const userToSave = await create(params);
+    await save(userToSave);
+    user = await findByEmail(params.email);
+    return user;
+  }
 }
